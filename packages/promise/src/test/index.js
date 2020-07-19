f0d800b5752f0b3253e3c48877471182537c1697
Li Pu <825369365@qq.com>
2020-07-19 22:21:15 +0800

    test(promise): add promise-aplus-test and rollup for test
    add promise-aplus-test and rollup to check the promise if is in A+ standard, #1

---
+++ packages/promise/src/test/index.js
@@ -0,0 +1,12 @@
+import Promise from "../promise/index";
+
+Promise.defer = Promise.deferred = function () {
+    let dfd = {};
+    dfd.promise = new Promise((resolve, reject) => {
+        dfd.resolve = resolve;
+        dfd.reject = reject;
+    });
+    return dfd;
+};
+
+export default Promise;
