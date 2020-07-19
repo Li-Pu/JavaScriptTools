1295e46a764cf0a5404a6608d063d50f3d136893
Li Pu <825369365@qq.com>
2020-07-19 20:13:43 +0800

    feat(promise): then method
    add support for params is not funciton and error handle, #1

--- packages/promise/src/promise/then.js
+++ packages/promise/src/promise/then.js
@@ -2,28 +2,52 @@ import Status from "./status";
 import resolvePromise from "./resolvePromise";
 
 export default function then(onFulfilled, onRejected) {
+    onFulfilled =
+        typeof onFulfilled === "function" ? onFulfilled : (value) => value;
+    onRejected =
+        typeof onRejected === "function"
+            ? onRejected
+            : (err) => {
+                  throw err;
+              };
     let promise2 = new Promise((resolve, reject) => {
         if (this.status === Status.FULFILLED) {
             setTimeout(() => {
-                let x = onFulfilled(this.value);
-                resolvePromise(promise2, x, resolve, reject);
+                try {
+                    let x = onFulfilled(this.value);
+                    resolvePromise(promise2, x, resolve, reject);
+                } catch (e) {
+                    reject(e);
+                }
             }, 0);
         } else if (this.status === Status.REJECTED) {
-            setTimeout(() => {
-                let x = onRejected(this.error);
-                resolvePromise(promise2, x, resolve, reject);
-            }, 0);
+            try {
+                setTimeout(() => {
+                    let x = onRejected(this.error);
+                    resolvePromise(promise2, x, resolve, reject);
+                }, 0);
+            } catch (e) {
+                reject(e);
+            }
         } else {
             this.onResolvedCallbacks.push(() => {
                 setTimeout(() => {
-                    let x = onFulfilled(this.value);
-                    resolvePromise(promise2, x, resolve, reject);
+                    try {
+                        let x = onFulfilled(this.value);
+                        resolvePromise(promise2, x, resolve, reject);
+                    } catch (e) {
+                        reject(e);
+                    }
                 }, 0);
             });
             this.onRejectedCallbacks.push(() => {
                 setTimeout(() => {
-                    let x = onRejected(this.error);
-                    resolvePromise(promise2, x, resolve, reject);
+                    try {
+                        let x = onRejected(this.error);
+                        resolvePromise(promise2, x, resolve, reject);
+                    } catch (e) {
+                        reject(e);
+                    }
                 }, 0);
             });
         }
