a2ee11f08bbf81e7b37e91f46b5990295885d3dc
Li Pu <825369365@qq.com>
2020-07-19 22:11:23 +0800

    feat(promise): add finally method
    add finally method in Promise, #1

--- packages/promise/src/promise/index.js
+++ packages/promise/src/promise/index.js
@@ -1,13 +1,26 @@
 import Promise from "./promise";
 import then from "./then";
-import resolve from './resolve';
-import reject from './reject';
-import all from './all';
-import race from './race';
+import resolve from "./resolve";
+import reject from "./reject";
+import all from "./all";
+import race from "./race";
 
 Promise.prototype.then = then;
 Promise.prototype.catch = function (onRejected) {
     return this.then(null, onRejected);
+};
+Promise.prototype.finally = function (callback) {
+    let constructor = this.constructor;
+    if (typeof callback === "function") {
+        return this.then(
+            (value) => constructor.resolve(callback()).then(() => value),
+            (error) =>
+                constructor.resolve(callback()).then(() => {
+                    throw error;
+                })
+        );
+    }
+    return this.then(callback, callback);
 };
 
 Promise.resolve = resolve;
