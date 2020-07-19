1f81c736f4d2cfbfe201071facb234e05675190c
Li Pu <825369365@qq.com>
2020-07-19 21:33:06 +0800

    feat(promise): catch method
    add catch method with then method, #1

--- packages/promise/src/promise/index.js
+++ packages/promise/src/promise/index.js
@@ -2,5 +2,8 @@ import Promise from "./promise";
 import then from "./then";
 
 Promise.prototype.then = then;
+Promise.prototype.catch = function (onRejected) {
+    return this.then(null, onRejected);
+};
 
 export default Promise;
